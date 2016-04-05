4e99ec50236c76289f591818c79c29ccca406b1a
Nick Cunningham <skarocks47@gmail.com>
2016-04-04 19:21:59 -0700

    Main File

---
+++ CullingCombatLog_1.0.ps1
@@ -0,0 +1,13 @@
+﻿$watchFile = "$env:USERPROFILE\appdata\Local\victory\Saved\Logs\victory.log"
+$console = $host.UI.RawUI
+$console.ForegroundColor = "white"
+$console.BackgroundColor = "black"
+cls
+Write-Host "Combat Log"
+
+Get-Content $watchFile -wait | Where-Object {$_ -match '.*You Hit.*|.*Struck by.*|.*ACH_.*'}| ForEach-Object { $_ -replace ".*:" }
+
+
+
+
+
